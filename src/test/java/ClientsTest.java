8e0d083de075b2dee747cad26357f4f5eb4a8635
CoderKnot <coder.knot@gmail.com>
2017-03-31 08:33:43 -0700

    add database rule for testing to setup test DB connection and delete rows on test completion

--- src/test/java/ClientsTest.java
+++ src/test/java/ClientsTest.java
@@ -1,20 +1,10 @@
-import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class ClientsTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
-  }
-
-  @After
-  public void tearDown() {
-    try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
-      con.createQuery(sql).executeUpdate();
-    }
-  }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
 }
